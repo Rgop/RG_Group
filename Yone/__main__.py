34c12fbf9a3bb0d52acb15740868958c74c4374c
Somyajeet Mishra <103039834+SJMxADITI@users.noreply.github.com>
2022-04-11 18:09:51 +0530

    Update __main__.py

--- Yone/__main__.py
+++ Yone/__main__.py
@@ -240,8 +240,8 @@ def yone_about_callback(update: Update, context: CallbackContext):
             reply_markup=InlineKeyboardMarkup(
                 [
                    [
-                     InlineKeyboardButton(text="Support", url="t.me/Yone_Support"),
-                     InlineKeyboardButton(text="News", url="t.me/Yone_Updates"),
+                     InlineKeyboardButton(text="Support", url="https://t.me/TrickyAbhii_Op"),
+                     InlineKeyboardButton(text="News", url="https://t.me/Techno_Trickop"),
                    ],
                    [
                     InlineKeyboardButton(text="Back", callback_data="yone_back")
